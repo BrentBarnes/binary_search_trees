88d6d06108bc95924bbae2fbb8754a5d2638894d
Brent Barnes <BrenttBarness@gmail.com>
2022-03-16 20:55:14 -0400

    Started level order method. Returns values of tree if no block given.

--- script.rb
+++ script.rb
@@ -18,19 +18,23 @@ module Comparable
     return root if value == root.data
     # return if root.nil?
 
+    # puts "Root: #{root.data}"
+    if !root.left_child.nil? then puts "Left child: #{root.left_child.data}" end
+    if !root.right_child.nil? then puts "Right child: #{root.right_child.data}" end
+
     if !root.left_child.nil? && root.left_child.data == value && parent ||
       !root.right_child.nil? && root.right_child.data == value && parent
-
+        
       root
     elsif value < root.data
       return root if root.left_child.nil?
-      p "Root: #{root.data}"
+      # p "Root: #{root.data}"
       root = root.left_child
       root = recursive_compare(root, value, parent)
       root
     elsif value > root.data
       return root if root.right_child.nil?
-      p "Root: #{root.data}"
+      # p "Root: #{root.data}"
       root = root.right_child
       root = recursive_compare(root, value, parent)
       root
@@ -85,21 +89,36 @@ class Tree
 
   def find(value)
     closest_node = recursive_compare(root, value)
-    puts "Found Node: #{closest_node.data}"
-    # return puts 'Node does not exist' if closest_node.data != value
+    # puts "Found Node: #{closest_node.data}"
+    return puts 'Node does not exist' if closest_node.data != value
 
       closest_node
   end
 
   def find_parent(value)
-    recursive_compare(root, value, true)
+    parent = recursive_compare(root, value, true)
+
+    if parent.data == value
+      nil
+    else
+      parent
+    end
+  end
+
+  def find_next_largest_node(node)
+    next_largest_node = node.right_child
+    until next_largest_node.left_child.nil?
+      next_largest_node = next_largest_node.left_child
+    end
+    puts "Next largest node is: #{next_largest_node.data}"
+    next_largest_node
   end
 
   def delete(value)
     node = find(value)
     puts "Node: #{node.data}"
     parent = find_parent(value)
-    puts "Parent: #{parent.data}"
+    # puts "Parent: #{parent.data}"
     left_or_right = compare_one(parent, node.data)
     children = 0
     if !node.left_child.nil? then children += 1 end
@@ -127,15 +146,60 @@ class Tree
       end
     end
 
-    
+    if children == 2
+    replacement_node = find_next_largest_node(node)
+    delete(replacement_node.data)
+    replacement_node.left_child = node.left_child
+    replacement_node.right_child = node.right_child
+    node.left_child = nil
+    node.right_child = nil
 
+      if parent.nil?
+        @root = replacement_node
+      elsif !left_or_right
+        parent.left_child = replacement_node
+      elsif left_or_right
+        parent.right_child = replacement_node
+      end
+    end
+  end
+
+  def level_order(queue=[], result =[], started=false, popped=root)
+    # queue = queue
+    show_queue = []
+    # result = result
+    queue.each {|item| show_queue << item.data}
+    puts "Show Queue: #{show_queue}"
+    puts "Popped: #{popped.data}"
+
+    if queue.empty? && started
+      # && popped.left_child.nil? && popped.right_child.nil?
+      puts "Basecase hit"
+      return
+    elsif !started
+      queue << popped
+      level_order(queue, result, true)
+    elsif !block_given?
+      popped = queue.pop
+      queue.unshift(popped.left_child) unless popped.left_child.nil?
+      queue.unshift(popped.right_child) unless popped.right_child.nil?
+      result << popped.data
+      level_order(queue, result, true, popped)
+    elsif block_given?
+      popped = queue.pop
+      queue.unshift(popped.left_child) unless popped.left_child.nil?
+      queue.unshift(popped.right_child) unless popped.right_child.nil?
+      result << yield(popped)
+      level_order(queue, result, true, popped)
+
+    end
+    result
   end
 end
 
 test = Tree.new(array)
 
+p test.level_order {|popped| puts popped.data}
 
-p test.delete(324)
-p test.find(67).right_child.data
 
 
